223b4266e70f55d1aa2fd0722aa27f01743bb235
Deb13579 <74122863+Deb13579@users.noreply.github.com>
2021-01-04 09:40:51 +0800

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,21 @@
+class Ball {
+    constructor(x, y, radius) {
+      var options = {
+        isStatic:false , 
+        restitution:1,
+        friction:0.1,
+        density:0.1
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      ellipseMode(RADIUS);
+      fill("black");
+      ellipse(pos.x, pos.y, this.radius);
+      pop();
+    }
+  };
